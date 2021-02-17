c669c25092473c993acb5d91a1024ae435cfcc26
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-02-17 23:32:05 +0100

    Styling a single vide page & updating other styles

--- videos/urls.py
+++ videos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.videos, name='videos'),
+    path('video', views.single_video, name="single_video"),
 ]
--- videos/views.py
+++ videos/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def videos(request):
     return render(request, 'videos/index.html')
+
+
+def single_video(request):
+    return render(request, 'videos/single_video.html')
