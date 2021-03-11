8c36a4367bc9ebbfeafb3e9ebf8ff7ca8fed93ee
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-03-11 14:37:05 +0100

    Adding a loading gif

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -41,3 +41,9 @@ function diasferoTimer(countDownDate){
     }
     }, 1000);
 }
+
+
+// Loading function
+function loading(){
+    document.querySelector(".loading").classList.add("hide-loading");
+}
