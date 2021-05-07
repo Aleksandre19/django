5e9d83238f514eff2f6493b027a54ef12253025b
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-05-07 17:59:12 +0200

    Retrieving a welcoming videos

--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from videos.models import Videos
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'home/index.html')
+
+    
+    welcoming_videos = Videos.objects.filter(welcoming=True)[:3]
+    new_video = Videos.objects.filter(new_video=True)[:1]
+
+    context = {
+        'welcoming_videos' : welcoming_videos,
+        'new_video' : new_video,
+    }
+
+    return render(request, 'home/index.html', context)
