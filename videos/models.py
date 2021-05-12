7b8d2781fe73a42a47482129648a53b54b756103
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-05-12 13:28:29 +0200

    Displaying side menu and videos on the videos page

--- videos/models.py
+++ videos/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    src = models.CharField(max_length=254, null=True, blank=True)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    src = models.CharField(max_length=254, null=True, blank=True)
+    
 
     def __str__(self):
         return self.name
@@ -13,17 +14,19 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Subjects(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254, null=True, blank=True)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    
 
     def __str__(self):
         return self.name
 
 
 class Videos(models.Model):
-    category_id = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     subjects = models.ForeignKey('Subjects', null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=254)
     content = models.URLField(max_length=1024)
--- videos/views.py
+++ videos/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Videos, Category, Subjects
+from django.db.models import Count
 # Create your views here.
 
 
 def videos(request):
-    if request.user.is_authenticated:
-        
+
+    ''' Checking to see if user is already athenticcated '''
+    if request.user.is_authenticated:  
+        # All Videos
         videos = Videos.objects.all()
-        subjects = Subjects.objects.all()
+        # All Subjects
+        subjects = Subjects.objects.all().annotate(posts_count=Count('videos'))
+        # All Categories
         categories = Category.objects.all()
+
+        # Defining variables
+        categorased = None
+        videos_by_subjects = None
+        videos_by_categories = None
+
+        if request.GET:
+            # Getting a videos by a category
+            if 'category' in request.GET:
+                categorased = request.GET['category'].split(',')
+                videos = videos.filter(category__name__in=categorased)
+                videos_by_categories = Category.objects.filter(name__in=categorased)
+            # Getting a videos by a Subjects
+            if 'subject' in request.GET:
+                categorased = request.GET['subject'].split(',')
+                videos = videos.filter(subjects__name__in=categorased)
+                videos_by_subjects = Subjects.objects.filter(name__in=categorased)
+
 
         context = {
             'videos' : videos,
             'categories' : categories,
             'subjects' : subjects,
+            'videos_by_categories' : videos_by_categories,
+            'videos_by_subjects' : videos_by_subjects,
         }
 
         return render(request, 'videos/index.html', context)
