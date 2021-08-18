0b2d767dd0e898271bf2e1d0778b8efa2819fb59
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-08-18 17:13:39 +0200

    Home's views - checking if a user has subscription

--- home/views.py
+++ home/views.py
@@ -1,54 +1,80 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse
 from videos.models import Videos, MyList, Likes, Category
 from videos.views import current_user_videos, get_videos_by_category
+from subscription.views import check_users_subscription
+from subscription.models import Cards
 
 # Create your views here.
 
-
+# This function displayes content on the home page for
+# both logged in and not logged in users 
 def index(request):
-
-    # Defining variables
-    mylist_videos = None
-    videos = None
-    videos_in_likes = None
-    videos_in_mylist = None
-    
-    # Requiring categories
-    categories = Category.objects.all()
-
-    # Requiring a weilcoming videos for home page
-    welcoming_videos = Videos.objects.filter(welcoming=True)[:3]
-    # Requiring a new video for home page ( Logged In User )
-    new_video = Videos.objects.filter(new_video=True)[:1]
 
     if request.user.is_authenticated:
 
-        # Requiring videos from MyList module by user
-        mylist_videos = MyList.objects.filter(user=request.user)
-        # Requiring videos
-        videos = Videos.objects.all()
-        
-        # Requiring videos from Likes and MyList modules by user
-        videos_in_likes = current_user_videos(Likes, request.user)
-        videos_in_mylist = current_user_videos(MyList, request.user)
+        # If a user is authenticated and has
+        # subscription so displaying the content
+        if check_users_subscription(request.user):
 
-        # Making a categories list
-        categories_list = []
-        for category in categories:
-            categories_list.append(category.name)
+            # Defining variables
+            mylist_videos = None
+            videos = None
+            videos_in_likes = None
+            videos_in_mylist = None
+            subscribed = None
+            
+            # Requiring categories
+            categories = Category.objects.all()
+
+            # Requiring a new video for home page ( Logged In User )
+            new_video = Videos.objects.filter(new_video=True)[:1]
+
+            # Requiring videos from MyList module by user
+            mylist_videos = MyList.objects.filter(user=request.user)
+            
+            # Requiring videos
+            videos = Videos.objects.all()
+            
+            # Requiring videos from Likes and MyList modules by user
+            videos_in_likes = current_user_videos(Likes, request.user)
+            videos_in_mylist = current_user_videos(MyList, request.user)
+
+            # Making a categories list
+            categories_list = []
+            for category in categories:
+                categories_list.append(category.name)
 
 
-    context = {
-        'welcoming_videos' : welcoming_videos,
-        'new_video' : new_video,
-        'categories' : categories,
-        'videos_by_categories' : get_videos_by_category(request, categories_list,
-                                                         Category, without_get=True) 
-                                                         if request.user.is_authenticated else None,
-        'mylist_videos' : mylist_videos,
-        'videos' : videos,
-        'videos_in_likes' : videos_in_likes,
-        'videos_in_mylist' : videos_in_mylist,
-    }
+            context = {
+                
+                'new_video' : new_video,
+                'categories' : categories,
+                'videos_by_categories' : get_videos_by_category(request, categories_list,
+                                                                Category, without_get=True) 
+                                                                if request.user.is_authenticated else None,
+                'mylist_videos' : mylist_videos,
+                'videos' : videos,
+                'videos_in_likes' : videos_in_likes,
+                'videos_in_mylist' : videos_in_mylist,
+                'subscribed' : subscribed,
+            }
+        # If a user has not subscription so
+        # redirecting to the subscription plans page 
+        else:
+            return redirect(reverse('cards'))
+    
+    # Then a user is not autheticated so 
+    # displaying the folowing content on the home page
+    else:
+
+        # Requiring a weilcoming videos for home page
+        welcoming_videos = Videos.objects.filter(welcoming=True)[:3]
+
+        cards = Cards.objects.all()
+
+        context = {
+            'welcoming_videos' : welcoming_videos,
+            'cards' : cards,
+        }
 
     return render(request, 'home/index.html', context)
