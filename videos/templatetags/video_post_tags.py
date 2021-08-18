74ddc9be08f86a262446de4c1489927f75f8c43c
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-08-18 17:31:55 +0200

    Updating video's simple_tag for subjects name

--- videos/templatetags/video_post_tags.py
+++ videos/templatetags/video_post_tags.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render, redirect
+""""
+    After clicking on the links located in the sidebar of the video's page,
+    catching URL of that and parssing it to get clear subject name.
+    Using this clear subject name in the side menu to hilight current subject's name
+    which was clicked. 
+"""
+
+# Importing template library
 from django import template
-from django.http import HttpRequest
-# from ..models import Subjects
-# import requests
 
 
+# Installing template library
 register = template.Library()
 
 
+# Creating a simple tag and parssing a current URL
 @register.simple_tag
 def current_subj_name(url=''):
     if "=" in url:
         subject_url = url.split('=')[1]
         return subject_url
+
+
+
+    
     
 
 
