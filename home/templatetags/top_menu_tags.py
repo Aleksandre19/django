3b6dd4b19ae57779200aaabbb19ed7d3ae426901
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-08-18 17:32:43 +0200

    Creating a simple_tag for subscription's checking

---
+++ home/templatetags/top_menu_tags.py
@@ -0,0 +1,23 @@
+"""
+    Checking if a user has already subscribed and
+    displaying a links Subscription or videos in the top main manu accordingly
+"""
+
+# Importing necessary modules
+from django import template
+from subscription.models import Subscription
+
+
+# Registering django template library
+register = template.Library()
+
+
+# Checking to see if a logged in user
+# already has subscription
+@register.simple_tag
+def check_subscription(user_id):
+    subscribed = None
+    subscription =  Subscription.objects.filter(user=int(user_id)) 
+    for sub in subscription:
+        subscribed = sub.subscribed
+    return subscribed
