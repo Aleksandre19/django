ae2b56e1c2116d7bf28af092e1d0cf32812eab80
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-08-18 17:30:03 +0200

    Updating subscription's admin.py

--- subscription/admin.py
+++ subscription/admin.py
@@ -6,24 +6,25 @@ from .models import Subscription, Cards
 class SubscriptionAdmin(admin.ModelAdmin):
     
 
-    readonly_fields = ('sub_number', 'date', 'user')
+    # readonly_fields = ('sub_number',)
 
 
-    fields = ('sub_number', 'user', 'full_name',
-              'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'date', 'price')
+    fields = ('sub_number', 'user', 'card', 'price',
+              'email', 'phone_number', 'start_date',
+              'end_date', 'subscribed')
 
 
-    list_display = ('full_name', 'user', 'email',
-                    'sub_number', 'date', 'price')
+    list_display = ('email', 'user', 'sub_number',
+                    'start_date', 'end_date', 'subscribed')
     
 
-    ordering = ('-date',)
+    ordering = ('email',)
 
 
 class CardsAdmin(admin.ModelAdmin):
-    fields = ('duration', 'price', 'saving', 'offering',)
+    fields = ('duration', 'price', 'saving', 'currency',
+             'stripe_plan_id', 'description', 'offering','tab_class',
+             'tab_aria',)
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
