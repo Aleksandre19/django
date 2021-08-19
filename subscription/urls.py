508e30a2ef00700839965bf281f8191b6a2ab447
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2021-08-19 22:06:40 +0200

    Creating new urls in subscription's urls.py

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('cards/', views.cards, name='cards'),
-    path('checkout', views.checkout, name='checkout'),
+    path('checkout/', views.checkout, name='checkout'),
+    # path('checkout_session', views.checkout_session, name='checkout_session'),
+    path('success/', views.sub_success, name='success'),
+    path('cancel/', views.sub_cancel, name='cancel'),
 ]
