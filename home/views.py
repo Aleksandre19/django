76c46e702d73f9c6cd9fa62a99066b150c0bb931
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2022-11-19 17:37:11 +0100

    Testuser - add  videos in likes & mylist.

--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ from videos.views import current_user_videos, get_videos_by_category
 from subscription.views import check_users_subscription
 from subscription.models import Cards
 from feedbacks.models import Feedbacks
+from django.contrib.auth.models import User
+import random
 
 # Create your views here.
 
@@ -18,7 +20,7 @@ def index(request):
         # If a user is authenticated and has
         # subscription so displaying the content
         if check_users_subscription(request.user):
-
+            
             # Defining variables
             mylist_videos = None
             videos = None
@@ -41,8 +43,35 @@ def index(request):
 
             # Getting latest feedback
             feedback = Feedbacks.objects.order_by('date').last()
+            
+            """ This code adds videos in to the likes and mylist
+                for the test user."""
+            # Get user name
+            test_user = request.user.username
+            if test_user == 'testme':
+                # Likes and mylist for test user.
+                user_likes = Likes.objects.filter(user=request.user)   
+                user_mylists = MyList.objects.filter(user=request.user)
+               
+                # Grab videos.
+                testuser_videos = videos[:10]
 
-            # Making a categories list
+                # Add videos in to the likes & mylists.
+                if not user_likes or not user_mylists:
+                    # Add in to the likes.
+                    if not user_likes:
+                        for video in random.choices(testuser_videos, k=3):
+                            add_liked_video = Likes(user=request.user, video=video)
+                            add_liked_video.save()
+
+                    # Add in to the MyList.
+                    if not user_mylists:
+                        # Add videos in to the mylist.
+                        for video in random.choices(testuser_videos, k=5):
+                            add_mylist_video = MyList(user=request.user, video=video)
+                            add_mylist_video.save()                        
+
+            # Make a categories list.
             categories_list = []
             for category in categories:
                 categories_list.append(category.name)
@@ -66,7 +95,18 @@ def index(request):
     # displaying the folowing content on the home page
 
     else:
-        # Requiring a weilcoming videos for home page
+
+        ''' Delete test user's likes and mylist '''
+        # Grab test user
+        testuser = User.objects.get(username='testme')
+        # Delete likes  
+        user_likes = Likes.objects.filter(user=testuser)
+        user_likes.delete()
+        # Delete mylist
+        user_mylist = MyList.objects.filter(user=testuser)
+        user_mylist.delete()
+
+        # Require weilcoming videos for home page.
         welcoming_videos = Videos.objects.filter(welcoming=True)[:3]
 
         cards = Cards.objects.all()
